ddaacab3f4dca0292fcf01ce9911fcfdd1d77149
Afifa-Tazremin-Oishi <afifatazreminoishi@gmail.con>
2022-02-17 21:23:28 +0600

    fifth

--- main.js
+++ main.js
@@ -5,19 +5,6 @@ function getAllValue(item){
      const inputValue = parseFloat(inputText);
      return inputValue;
 }
-function getPositiveNumber(){
-    const positiveNumber = inputValue;
-    const successMessage = document.getElementById('notify-success');
-    const failError = document.getElementById('notify-failed');
-    if(positiveNumber>0){
-       successMessage.style.display = 'block';
-       failError.style.display = 'none';
-    } 
-    else{
-        successMessage.style.display = 'none';
-        failError.style.display = 'block';
-    }
-}
 
 function getExpenses(){
     const incomeValue = getAllValue('income');
@@ -27,15 +14,41 @@ function getExpenses(){
     const clothValue = getAllValue('cloth');
     const total = foodValue + rentValue + clothValue;
     const balance = incomeValue - total;
+    
+    //error handling
+    if(incomeValue<0 || foodValue<0 || rentValue<0 || clothValue<0){
+        const invalid = document.getElementById('notify-failed');
+        invalid.style.display = 'block';
+    }
+    else if(total>incomeValue){
+        const errorMessage = document.getElementById('notify-error');
+        errorMessage.style.display = 'block';
+    }
+    else{
     document.getElementById('total-expence').innerText = total;
     document.getElementById('total-balance').innerText = balance;
-    //for saving amount
+
+    }
+    return balance;
+
+}
+function percentageBalance(){
+    const incomeValue = getAllValue('income');
     const saveValue = getAllValue('save');
     const saveAmount = (incomeValue/100)*saveValue;
-    document.getElementById('total-save').innerText = saveAmount;
+    
     //for remaining balance
-    const remainingBalance = balance - saveAmount;
-    document.getElementById('total-remaining').innerText = remainingBalance;
+    const totalBalance = getExpenses();
+    const remainingBalance = totalBalance - saveAmount;
+    
+    if(remainingBalance<0){
+       const showError = document.getElementById('notify-wrong');
+       showError.style.display = 'block';
+    }
+    else{
+        document.getElementById('total-save').innerText = saveAmount;
+        document.getElementById('total-remaining').innerText = remainingBalance;
+    }
 
 }
 //handle calculation btn eventlistener
@@ -44,5 +57,5 @@ document.getElementById('calculation-btn').addEventListener('click', function(){
 });
 //handle save btn eventlistener
 document.getElementById('save-btn').addEventListener('click', function(){
-    getExpenses();
-})
+    percentageBalance();
+});
